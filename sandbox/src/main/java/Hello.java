4f0c9944a0ab1bd16fc2a7c02df763416159c816
KaterinaScherbakova <arktik.katrine@gmail.com>
2026-04-28 16:52:58 +0300

    try-catch sections

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,5 +1,28 @@
+import java.io.File;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        try{
+            var z = calculate();
+            System.out.println(z);
+            System.out.println("Hello World!");
+        }catch(ArithmeticException exception){
+            exception.printStackTrace();
+        }
+
     }
+
+    private static int calculate() {
+        var x=1;
+        var y=1;
+        var z = divide(x, y);
+        return z;
+    }
+
+    private static int divide(int x, int y) {
+        var z = x / y;
+        return z;
+    }
+
 }
--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,6 +1,15 @@
 package ru.stqa.geometry.figures;
 
 public class Rectangle {
+
+    private double a;
+    private double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
     //площадь прямоугольника
     public static void printRectangleArea(double a, double b) {
         var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a,b,rectangleArea(a,b));
